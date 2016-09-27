039e052d77a46b1d439e78cde3a1ad72e5230eee
Patrick Farrell <patrick.farrell@grandrounds.com>
2016-09-26 20:08:53 -0700

    login working

--- app/routes/login.rb
+++ app/routes/login.rb
@@ -1,7 +1,5 @@
 class Moth < Sinatra::Application
   post "/login" do
-    require 'byebug'
-    byebug
     user = User.find(email: params[:email])
     (user && User.login(params[:email], params[:password])) ? "success" : "fail"
   end
--- app/routes/user.rb
+++ app/routes/user.rb
@@ -6,6 +6,8 @@ class Moth < Sinatra::Application
 
   post "/user" do
     user = User.new(email: params[:email], name: params[:name])
+    user.set_password(params[:password])
+    user.save
     redirect(url_for("/"))
   end
 end
